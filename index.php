85ca922510e82cd921fdd9071bb08c7dce2cc498
techycat <techycat7@gmail.com>
2015-04-12 19:39:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<title>Math Game</title>
-</head>
-<body>
-<h1>Testing</h1>
-</body>
-</html>
+<?php
+echo "hai this is NITIN";
+?>
